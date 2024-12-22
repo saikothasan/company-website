37b383c4308ece9fb1b81dc96f13c2bd58eb27bd
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2024-12-22 19:01:22 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Hero } from './components/sections/Hero'
 import { Features } from './components/Features'
-import { Navbar } from './components/Navbar'
 import { Testimonials } from './components/sections/Testimonials'
 import { Pricing } from './components/sections/Pricing'
 import { CTA } from './components/sections/CTA'
@@ -8,7 +7,6 @@ import { CTA } from './components/sections/CTA'
 function App() {
   return (
     <main className="bg-gray-900">
-      <Navbar />
       <Hero />
       <Features />
       <Testimonials />
